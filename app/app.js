bd2ad54bb95b371e5e666381dbac84d990552959
Nikita Ram <nikitaram2799@gmail.com>
2026-03-26 17:36:00 +0530

    project code

---
+++ app/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello DevOps 🚀");
+});
+
+app.get("/version", (req, res) => {
+  res.json({ version: "1.0.0" });
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
